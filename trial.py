a811956e2f4c069a45e451dabd15bc38d7eafb1d
coral.buthello@hughes.com <coral.buthello@hughes.com>
2019-06-24 11:21:19 -0400

    save

---
+++ trial.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+my_list=['192.168.1.10', 5080, 'up']
+print('the first:'+my_list[0])
+print ('second item:'+str(my_list[1]))
+print ('thrisd:'+my_list[2])
+new_list=[5080,'80', 55, '10.0.0.1', '10.20.30.1', 'ssh']
+#print('when i' +new_list[5] 'into IP addresse' +new_list[3] 'or' +new_list[4] 'i am unable to ping ports' +new_list[0] ',' +new_lst[1] 'or' +new+list[2])
+print(f"when i ssh into ip address{new_list[3]}or {new_list[4]} i am unable to ping ports{new_list[0]},{new_list[1]}, or {new_list[2]}.")
